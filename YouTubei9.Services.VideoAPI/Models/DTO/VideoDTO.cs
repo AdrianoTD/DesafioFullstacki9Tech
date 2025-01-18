45410b6faae01dcf64c6c08f2253aa5f5e8d8cbf
Adriano T. Dotta <adrianot.dotta@gmail.com>
2025-01-18 19:59:49 -0300

    Criação e definição inicial dos arquivos de estrutura do vídeo

---
+++ YouTubei9.Services.VideoAPI/Models/DTO/VideoDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace YouTubei9.Services.VideoAPI.Models.DTO
+{
+    public class VideoDTO
+    {
+        public string Kind { get; set; }
+        public string Etag { get; set; }
+    }
+}
---
+++ YouTubei9.Services.VideoAPI/Models/Video.cs
@@ -0,0 +1,8 @@
+﻿namespace YouTubei9.Services.VideoAPI.Models
+{
+    public class Video
+    {
+        public string Kind { get; set; }
+        public string Etag { get; set; }
+    }
+}
